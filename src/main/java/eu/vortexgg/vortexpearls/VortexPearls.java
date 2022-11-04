df4410265371d048d7838d43da91ea62478556c8
xVorttex <sigamatute@gmail.com>
2022-11-04 23:15:55 +0200

    v6.0

--- src/main/java/eu/vortexgg/vortexpearls/VortexPearls.java
+++ src/main/java/eu/vortexgg/vortexpearls/VortexPearls.java
@@ -1,15 +1,8 @@
 package eu.vortexgg.vortexpearls;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentMap;
-
 import eu.vortexgg.vortexpearls.handler.CooldownHandler;
-import org.bukkit.Material;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.scheduler.BukkitTask;
-
 import eu.vortexgg.vortexpearls.hook.Hook;
+import org.bukkit.entity.Player;
 
 public class VortexPearls {
     public static boolean LAZARUS_ENABLED = false;
--- src/main/java/eu/vortexgg/vortexpearls/hook/Hook.java
+++ src/main/java/eu/vortexgg/vortexpearls/hook/Hook.java
@@ -1,6 +1,5 @@
 package eu.vortexgg.vortexpearls.hook;
 
-import eu.vortexgg.vortexpearls.VortexPearls;
 import org.bukkit.entity.Player;
 
 public interface Hook {
